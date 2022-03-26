317c2c4b12b7da13d1b7da9f75c5f6b6b6ca26fe
sangbinlee <sangbinlee9@gmail.com>
2022-03-26 12:26:49 +0900

    first

---
+++ src/test/java/com/sodi/store/common/StoreCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sodi.store.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
